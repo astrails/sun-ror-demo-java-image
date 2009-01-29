2c2ebb118347818253469431a2e8a2750ef4d020
Vitaly Kushner <vitaly-at-astrails-dot-com>
2009-01-29 16:36:38 +0200

    use resource_controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,94 +1,28 @@
 class PhotosController < ApplicationController
-  # GET /photos
-  # GET /photos.xml
-  def index
-    @photos = Photo.find(:all)
+  resource_controller
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @photos }
+  index.wants.xml { render :xml => @photos }
+
+  show do
+    wants.xml { render :xml => @photo }
+    wants.jpg do
+      data = params[:bw] ? @photo.bw_image : @photo.binary_data
+      send_data(data, :type => @photo.content_type, :filename => @photo.filename, :disposition => 'inline')
     end
   end
 
-  # GET /photos/1
-  # GET /photos/1.xml
-  # GET /photos/1.jpg
-  # GET /photos/1.jpg?bw=true
-  def show
-    @photo = Photo.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @photo }
-      format.jpg {
-        data = params[:bw] ? @photo.bw_image : @photo.binary_data
-        
-        send_data(data, :type => @photo.content_type, :filename => @photo.filename, :disposition => 'inline')
-      }
-    end
+  create do
+    flash = "Photo was successfully created."
+    wants.xml { render :xml => @photo, :status => :created, :location => @photo }
+    failure.wants.xml { render :xml => @photo.errors, :status => :unprocessable_entity }
   end
 
-  # GET /photos/new
-  # GET /photos/new.xml
-  def new
-    @photo = Photo.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @photo }
-    end
+  update do
+    flash 'Photo was successfully updated.'
+    wants.xml { head :ok }
+    failure.wants.xml { render :xml => @photo.errors, :status => :unprocessable_entity }
   end
 
-  # GET /photos/1/edit
-  def edit
-    @photo = Photo.find(params[:id])
-  end
-
-  # POST /photos
-  # POST /photos.xml
-  def create
-    @photo = Photo.new(params[:photo])
-
-    respond_to do |format|
-      if @photo.save
-        flash[:notice] = "Photo was successfully created."
-        format.html { redirect_to(@photo) }
-        format.xml  { render :xml => @photo, :status => :created, :location => @photo }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /photos/1
-  # PUT /photos/1.xml
-  def update
-    @photo = Photo.find(params[:id])
-
-    respond_to do |format|
-      if @photo.update_attributes(params[:photo])
-        flash[:notice] = 'Photo was successfully updated.'
-        format.html { redirect_to(@photo) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /photos/1
-  # DELETE /photos/1.xml
-  def destroy
-    @photo = Photo.find(params[:id])
-    @photo.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(photos_url) }
-      format.xml  { head :ok }
-    end
-  end
-
+  destroy.wants.xml { head :ok }
 
 end
